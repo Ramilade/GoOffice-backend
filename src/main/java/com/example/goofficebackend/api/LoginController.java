c26c7e415a4f248e7e7af09900d4b0989f5b8ada
FHallengreen <fred70p9@stud.kea.dk>
2023-05-08 22:10:35 +0200

    changes to security

--- src/main/java/com/example/goofficebackend/api/LoginController.java
+++ src/main/java/com/example/goofficebackend/api/LoginController.java
@@ -2,6 +2,8 @@ package com.example.goofficebackend.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +15,20 @@ import java.util.Map;
 @RestController
 public class LoginController {
 
+
     @GetMapping("/auth-status")
     public ResponseEntity<Map<String, Object>> authStatus(Authentication authentication) {
         Map<String, Object> response = new HashMap<>();
-        response.put("isAuthenticated", authentication != null && authentication.isAuthenticated());
+        boolean isAuthenticated = authentication != null && authentication.isAuthenticated();
+        response.put("isAuthenticated", isAuthenticated);
+
+        if (isAuthenticated) {
+            OAuth2User oauth2User = ((OAuth2AuthenticationToken) authentication).getPrincipal();
+            String email = oauth2User.getAttribute("email");
+            response.put("email", email);
+        }
+
         return ResponseEntity.ok(response);
     }
+
 }
