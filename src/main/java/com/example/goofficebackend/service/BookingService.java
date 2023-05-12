1e02d9e467770556126126c4c9b7b34be8f878f3
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2023-05-12 09:31:13 +0200

    fixed small bug with booking of morning shifts

--- src/main/java/com/example/goofficebackend/service/BookingService.java
+++ src/main/java/com/example/goofficebackend/service/BookingService.java
@@ -52,7 +52,8 @@ public class BookingService {
       throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Start and end date is not the same");
     }
     // check if startdate or enddate is not before present
-    if (shiftStart.isBefore(LocalDateTime.now()) || shiftEnd.isBefore(LocalDateTime.now())) {
+    LocalDate shiftStartDate = shiftStart.toLocalDate();
+    if (shiftStartDate.isBefore(LocalDate.now()) || shiftEnd.isBefore(LocalDateTime.now())) {
       throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Date is before present");
     }
     // check if enddate is more than 4 weeks in the future
